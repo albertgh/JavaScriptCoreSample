63b7d686712d1b384586bf79eb5dc594525c93d9
Albert Chu <albertzyk@gmail.com>
2014-04-04 14:56:44 +0800

    Initial Commit

---
+++ JavaScriptCoreSampleTests/JavaScriptCoreSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  JavaScriptCoreSampleTests.m
+//  JavaScriptCoreSampleTests
+//
+//  Created by Albert Chu on 14-4-4.
+//  Copyright (c) 2014年 Albert Chu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JavaScriptCoreSampleTests : XCTestCase
+
+@end
+
+@implementation JavaScriptCoreSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
